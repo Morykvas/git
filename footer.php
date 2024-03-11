324729882bbab18cb8af02dbd777c8bdd22a07f7
Dmytro Morykvas <morykvasdm@gmail.com>
2024-03-11 10:49:04 +0200

    зміни з повідомлення на сайті для юрка

--- footer.php
+++ footer.php
@@ -5,6 +5,7 @@
                     <div class="wrapper-footer">
                         <h1>FOOTER</h1>
                         <h2>Дмитрик скоро піде в гугл</h2>
+                        <p>я ще добре подумаю чи туда треба йти, бо там ще Юрчика не має </p>
                     </div>
                 </div>
                 <script src="<?= SCRIPT_DIR; ?>min.js"></script>
